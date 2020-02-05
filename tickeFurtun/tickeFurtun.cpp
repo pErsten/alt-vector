6476d228a4a40d7437324b624e0afaf36109ec91
Vlad Paplinsky <vladosmros@outlook.com>
2020-02-05 11:54:02 +0200

    .

---
+++ tickeFurtun/tickeFurtun.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <cmath>
+#include <vector>
+#include "vec.h"
+
+int main() {
+	vec<int> a(3);
+	a[0] = 5;
+	a[1] = 6;
+	a[2] = 89;
+	a.addBack(8970);
+	a.addBack(44);
+	a.addBack(474);
+	a.addBack(445);
+	a.addBack(445);
+	a.addBack(445);
+	a.addBack(445);
+	a.addBack(445);
+	a.addBack(424);
+	a.addBack(4);
+	a.deleteBack();
+	a.addBack(78);
+	a.arrSort(-1);
+	std::cout << a;
+	std::cout << std::endl;
+	std::cout << a.find(445, 0) << " " << a.find(445, -1) << " " << a.find(445);
+	std::cout << std::endl;
+	vec<std::string> b(2);
+	b[0] = "My";
+	b[1] = "name";
+	b.addBack("is");
+	b.addBack("Vlad");
+	b.arrSort();
+	std::cout << b;
+	std::cout << std::endl;
+	vec<char> c(4);
+	c[0] = 'V';
+	c[1] = 'l';
+	c[2] = 'a';
+	c[3] = 'd';
+	c.arrSort();
+	std::cout << c << std::endl;
+	int row = 50;
+	int col = 10;
+	vec<vec<int>> d(row);
+	d[0] = vec<int>(col);
+	d[1] = vec<int>(col);
+	d[0][2] = 7;
+	d[1][3] = 8;
+	std::cout << d[0][2] << " " << d[1][3];
+	for (int i = 0; i < row; i++) {
+		d[i] = vec<int>(col);
+		for (int j = 0; j < col; j++) {
+			d[i][j] = i*j;
+		}
+	}
+	d[2].arrSort();
+	for (int i = 0; i < row; i++) {
+		std::cout << std::endl;
+		for (int j = 0; j < col; j++) {
+			std::cout << d[i][j] << " ";
+		}
+	}
+	//std::cout << d[0][3];
+	//std::vector<int> Tchlaine(1000), h;
+	//std::cout << sizeof Tchlaine << " " << sizeof h;
+	std::cout << "hgkdh";
+	//vec<int> t = { 0,9,7 };
+}
---
+++ tickeFurtun/vec.h
@@ -0,0 +1,128 @@
+#pragma once
+#include <algorithm>
+template<class C>
+class vec {
+	int size;
+	int currpos;
+	C* arr;
+public:
+	vec(const int &num = 0) : size(num), currpos(num - 1)
+	{
+		if (size < 0) {
+			std::cerr << "Error: you're too dumb to use this class";
+			abort();
+		}
+		arr = new C[size];
+	}
+	void addBack(const C &value) {
+		if (++currpos >= size) {
+			C* newArr = new C[size + 50];
+			/*for (int i = 0; i < size; i++) {
+				newArr[i] = arr[i];
+			}
+			int i = 0;
+			for (C* pc = arr, *pl = arr + size; pc < pl; ++pc, ++i) {
+				*(newArr + i) = *pc;
+			}*/
+			memcpy(newArr, arr, size*sizeof(*arr));
+			//realloc(arr, (size+50)*sizeof(*arr));
+			delete[] arr;
+			arr = newArr;
+			size += 50;
+		}
+		*(arr + currpos) = value;
+		size++;
+	}
+	void deleteBack() {
+		arr[currpos--] = NULL;
+	}
+	const int& length() {
+		return currpos + 1;
+	}
+	const int& reserved() {
+		return size;
+	}
+	void reserve(const int &newSize) {
+		if (newSize < 0) {
+			std::cerr << "\nError: can't reserve a negative number";
+			return; 
+		}
+		C* newArr = new C[currpos + 1 + newSize];
+		memcpy(newArr, arr, size * sizeof(*arr));
+		delete[] arr;
+		arr = newArr;
+		size = newSize;
+	}
+	const int& find(const C &value, const int &startOrEnd = 0) {
+		if (startOrEnd >= 0)
+			for (C *pc = arr, *pl = arr + currpos + 1; pc < pl; ++pc) {
+				if (*pc == value)
+					return pc - arr;
+			}
+		else
+			for (C *pc = arr + currpos + 1, *pl = arr; pc >= pl; pc--) {
+				if (*pc == value)
+					return pc - arr;
+			}
+		return -1;
+	}
+	~vec() {
+		delete[] arr;
+	}
+	void arrSort(const int &type = 0) {
+		std::sort(arr, arr + currpos + 1, [&](const C& a, const C& b) -> bool
+			{
+				return ((type >= 0) ? a > b : a < b);
+			});
+	}
+	C& operator[](const int &index) {
+		try {
+			if (index <= currpos && index >= 0)
+				return *(arr + index);
+			else if (index < 0 && currpos >= -index)
+				return *(arr + currpos + index + 1);
+			else
+				throw (std::string)"\nError: wrong access to memory";
+		}
+		catch (std::string e) {
+			std::cerr << e;
+			abort();
+		}
+		catch (...) {
+			std::cerr << "\nError: unidentified error";
+			abort();
+		}
+	}
+	vec<C>& operator=(const vec<C> &vector) {
+		this->size = vector.size;
+		this->arr = new C[size];
+		this->currpos = vector.currpos;
+		return *this;
+	}
+	friend std::ostream& operator<<(std::ostream& out, const vec& thes) {
+		for (int i = 0; i < thes.currpos; i++) {
+			out << thes.arr[i] << ", ";
+		}
+		out << thes.arr[thes.currpos];
+		return out;
+	}
+};
+template<>
+void vec<char>::arrSort(const int &type) {
+	std::sort(arr, arr + currpos + 1, [&](char a, char b) -> bool
+		{ 
+			if ((int)(a) >= 65 && (int)(a) <= 90) a += 32;
+			if ((int)(b) >= 65 && (int)(b) <= 90) b += 32;
+			return ((type >= 0) ? a < b : a > b);
+		});
+}
+void vec<std::string>::arrSort(const int &type) {
+	std::sort(arr, arr + currpos + 1, [&](std::string a, std::string b) -> bool
+		{
+			for (int i = 0; i < a.length(); i++)
+				if ((int)(a[i]) >= 65 && (int)(a[i]) <= 90) a[i] += 32;
+			for (int i = 0; i < b.length(); i++)
+				if ((int)(b[i]) >= 65 && (int)(b[i]) <= 90) b[i] += 32;
+			return ((type >= 0) ? a < b : a > b);
+		});
+}
